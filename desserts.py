b6f09b28dcc00c0a3aac59ea004532f7437ecd51
Chantel Yip <chantelyip422@gmail.com>
2020-05-12 00:12:20 +0000

    created a dictionary to store all cupcake instances by name, instance attributes, and instance methods

--- desserts.py
+++ desserts.py
@@ -4,13 +4,36 @@
 class Cupcake:
     """A cupcake."""
 
+    cache = {}
+
     def __repr__(self):
         """Human-readable printout for debugging."""
 
-        
-
-
         return f'<Cupcake name="{self.name}" qty={self.qty}>'
+
+    def __init__(self, name, flavor, price):
+
+        self.name = name
+        self.flavor = flavor
+        self.price = price
+        self.qty = 0
+        self.cache[name] = self
+
+    def add_stock(self,amount):
+        self.qty += amount
+
+    def sell(self,amount):
+      if self.qty == 0:
+          print('Sorry, these cupcakes are sold out')
+          return
+
+      if self.qty < amount:
+          self.qty = 0
+          return
+
+      self.qty -= amount
+
+      
 
 if __name__ == '__main__':
     import doctest
